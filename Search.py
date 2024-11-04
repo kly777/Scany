e27d42cbc1ff933bc9aa60adcc914f8503a22d32
kly777 <kly777777777@gmail.com>
2024-11-04 16:07:32 +0800

    first commit

---
+++ Search.py
@@ -0,0 +1,146 @@
+from DrissionPage import ChromiumPage
+from DrissionPage import Chromium
+
+import os
+browser = Chromium()
+def get_g_url(q):
+    try:
+        page = browser.new_tab(None)
+        print("G START")
+        page.get('https://www.google.com')
+        page('#APjFqb').input(q)
+        page.actions.key_down('enter')
+        page.wait.load_start()
+        result_container = page.eles("@class=dURPMd")
+        results = result_container[0].children("tag=div")
+
+        result = []
+        for i in range(len(results)):
+            p = results[i].ele("tag=a")
+            if p:
+                result.append(p.attr("href"))
+        print("G END")
+        page.close()
+        return result
+    except:
+        print("G ERROR")
+        return []
+
+
+
+def get_b_url(q):
+    try:
+        page=browser.new_tab(None)
+        print("B START")
+        page.get('https://www.bing.com')
+        page('@id=sb_form_q').input(q)
+        page.actions.click(on_ele=page('@id=sb_form_q'))
+        page.actions.key_down('enter')
+
+        page.wait.load_start()
+        result_container = page.eles("@id=b_results")
+        results = result_container[0].children("tag=li")
+
+        result = []
+        for i in range(len(results)):
+            p = results[i].ele("tag=a")
+            if p:
+                result.append(p.attr("href"))
+        print("B END")
+        page.close()
+        return result
+    except:
+        print("B ERROR")
+        return []
+
+def get_d_url(q):
+    page = browser.new_tab(None)
+    print("D START")
+    page.get('https://www.duckduckgo.com')
+    page('@id=searchbox_input').input(q)
+    page.actions.click(on_ele=page('@id=searchbox_input'))
+    page.actions.key_down('enter')
+
+    page.wait.load_start()
+    result_container = page.eles("@class=react-results--main")
+    results = result_container[0].children("tag=li")
+
+    result = []
+    for i in range(len(results)):
+        p = results[i].eles("tag=a")[1]
+        if p:
+            result.append(p.attr("href"))
+    print("D END")
+    page.close()
+    return result
+
+
+def get_ba_url(q):
+    page = browser.new_tab(None)
+    print("BA START")
+    page.get('https://www.baidu.com')
+    page('@id=kw').input(q)
+    page.actions.click(on_ele=page('@id=kw'))
+    page.actions.key_down('enter')
+
+    page.wait.load_start()
+    result_container = page.eles("@id=content_left")
+    results = result_container[0].children("tag=div")
+
+    result = []
+    for i in range(len(results)):
+        p = results[i].eles("tag=a")[1]
+        if p:
+            result.append(p.attr("href"))
+    print("BA END")
+    page.close()
+    return result
+
+
+def get_y_url(q):
+    page = browser.new_tab(None)
+    print("Y START")
+    page.get('https://search.yahoo.com')
+    page('@id=yschsp').input(q)
+    page.actions.click(on_ele=page('@id=yschsp'))
+    page.actions.key_down('enter')
+
+    page.wait.load_start()
+    result_container = page.eles("@class=reg  searchCenterMiddle")
+    results = result_container[0].children("tag=li")
+
+    result = []
+    for i in range(len(results)):
+        p = results[i].ele("tag=a")
+        if p:
+            result.append(p.attr("href"))
+    print("Y END")
+    page.close()
+    return result
+
+
+def get_s_url(q):
+    try:
+        page = browser.new_tab(None)
+        print("S START")
+        page.get('https://startpage.com')
+        page('@id=q').input(q)
+        page.actions.click(on_ele=page('@id=q'))
+        page.actions.key_down('enter')
+
+        page.wait.load_start()
+        result_container = page.eles("@class=w-gl")
+        results = result_container[0].children("tag=div")
+
+        result = []
+        for i in range(len(results)):
+            p = results[i].ele("tag=a")
+            if p:
+                result.append(p.attr("href"))
+        print("S END")
+        page.close()
+        return result
+    except:
+
+        print("S ERROR")
+        return []
---
+++ init.py
@@ -0,0 +1,26 @@
+from Search import get_g_url, get_b_url, get_d_url, get_ba_url, get_y_url, get_s_url
+from collections import Counter
+from concurrent.futures import ThreadPoolExecutor
+q = 'vue ui library'
+
+with ThreadPoolExecutor() as executor:
+    futures = [executor.submit(func, q) for func in [
+        get_b_url, get_d_url, get_g_url, get_y_url, get_ba_url, get_s_url]]
+    all_urls = sum((future.result() for future in futures), [])
+print("collected")
+url_counter = Counter(all_urls)
+
+# 将结果根据出现次数排序
+sorted_urls = sorted(url_counter.items(),
+                     key=lambda item: item[1], reverse=True)
+print("已排序")
+# 写入文件
+web_count = 0
+
+with open('sorted_urls.txt', 'w', encoding="utf-8") as file:
+    for url, count in sorted_urls:
+        file.write(f"{url}: {count}\n")
+        web_count += count
+
+print("已输出到文件sorted_urls.txt")
+print("共爬取到", web_count, "条结果")
